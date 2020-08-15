f9bb2335dd7061986ec5ec3a62ef68fa6a438b60
syljoy <syljoy@163.com>
2020-08-15 18:21:44 +0800

    commit 20200202

---
+++ 2020-02-02/cplus.cpp
@@ -0,0 +1,10 @@
+#include "lover.h"
+
+Dear Highwn
+	You are the_girl in my_dream;	
+	You are indispensible to_me;
+	I love you, with all my heart and soul, always_and_forever;
+
+										Love forever;
+										2020-02-02;
+										syl
---
+++ 2020-02-02/lover.h
@@ -0,0 +1,25 @@
+#include <climits>
+#include <iostream>
+#define in )
+#define all (
+#define syl }
+#define with I
+#define love (
+#define You int
+#define my "µÄ"
+#define are main
+#define I printf
+#define and "%d"
+#define to_me 1493
+#define the_girl (
+#define my_dream {
+#define heart "µÚ"
+#define Highwn std;
+#define Love return
+#define you "°®Äã")
+#define soul "Ìì\n"
+#define indispensible =
+#define forever INT_MAX
+#define Dear using namespace
+#define always_and_forever to_me)
+
